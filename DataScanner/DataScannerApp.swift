cb1df65a71749659a00dc35de34988300f7d9785
BoringLadder <dustinhitzing@gmail.com>
2023-12-02 19:25:04 -0500

    Initial Commit

---
+++ DataScanner/DataScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataScannerApp.swift
+//  DataScanner
+//
+//  Created by Dustin Hitzing on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct DataScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
